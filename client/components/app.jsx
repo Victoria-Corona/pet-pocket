854349a0157be8bc1383cf155d02290315c79b46
Jenna Bowman <jbowman1@swarthmore.edu>
2020-08-05 01:04:58 +0000

    user can view homepage front end

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
+import Homepage from './homepage';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      message: null,
-      isLoading: true
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
   }
 
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+    return (
+      <>
+        <Homepage />
+      </>
+    );
   }
 }
