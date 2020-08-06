f3fee585e6881fe753998b8a7ca285e5bd482f69
Jenna Bowman <jbowman1@swarthmore.edu>
2020-08-06 06:16:51 +0000

    Updated some small css issues

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,30 +1,48 @@
 import React from 'react';
-//import Homepage from './homepage';
+import Homepage from './homepage';
 import MainMenu from './mainMenu';
 import ProfileList from './profile-list';
-
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      view: {
+        name: 'homepage',
+        params: {}
+      }
+    };
+    this.setView = this.setView.bind(this);
   }
 
   componentDidMount() {
   }
 
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
+  }
+
   render() {
+    const view = this.state.view.name;
+    let renderPage;
+    if (view === 'homepage') {
+      renderPage = <Homepage setView={this.setView} />;
+    } else if (view === 'mainMenu') {
+      renderPage = <MainMenu setView={this.setView} />;
+    } else if (view === 'profileList') {
+      renderPage = <ProfileList setView={this.setView} />;
+    }
     return (
       <>
-        <MainMenu />
+        <div className="container">
+          {renderPage}
+        </div>
       </>
-      //    return this.state.isLoading
-      // ? <h1>Testing connections...</h1>
-      // : <h1>{this.state.message.toUpperCase()}</h1>;
-      <div>
-        <ProfileList/>
-      </div>
-
     );
   }
 }
