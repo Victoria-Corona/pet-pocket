cc0eb7da5ef10440a0cc4c53ef38b7e38f9ab08a
VicCoro <corona.victoria@hotmail.com>
2020-08-06 12:10:27 -0700

    user can switch between views on pet profile and pet profile lists, some styling for navigation purpose

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,11 +6,14 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      message: null,
+      isLoading: true,
       view: {
-        name: 'details',
+        name: 'list',
         params: {}
       }
     };
+    this.setView = this.setView.bind(this);
   }
 
   componentDidMount() {
@@ -22,16 +25,24 @@ export default class App extends React.Component {
   }
 
   setView(name, params) {
+    const newView = {
+      name: name,
+      params: params
+    };
+
+    this.setState({
+      view: newView
+    });
   }
 
   render() {
     if (this.state.view.name === 'list') {
       return (
-        <ProfileList/>
+        <ProfileList setView={this.setView} />
       );
     } else {
       return (
-        <ProfileDetails/>
+        <ProfileDetails params={this.state.view.params} setView={this.setView}/>
       );
     }
   }
--- client/components/profile.jsx
+++ client/components/profile.jsx
@@ -6,25 +6,18 @@ class ProfileDetails extends React.Component {
     this.state = {
       pet: null
     };
-    this.getProfile = this.getProfile.bind(this);
   }
 
   componentDidMount() {
-    this.getProfile();
-  }
-
-  getProfile(petId) {
-    fetch(`/api/petProfile/${petId}`, {
-      method: 'GET'
-    })
-      .then(res => res.json()).then(data => {
-        this.setState({ pet: data });
-      })
+    fetch(`/api/petProfile/${this.props.params.petId}`)
+      .then(res => res.json())
+      .then(data => this.setState({ pet: data }))
       .catch(err => console.error(err.message));
   }
 
   render() {
     if (!this.state.pet) return null;
+
     const birthday = this.state.pet.dateOfBirth;
     const date = new Date(birthday);
     const options = {
@@ -36,20 +29,25 @@ class ProfileDetails extends React.Component {
 
     return (
       <div>
+        <div className="p-4 header-background d-flex justify-content-between align-items-center">
+          <i className="fas fa-edit fa-lg"></i>
+          <span className="title text-uppercase">PET POCKET</span>
+          <i className="fas fa-undo-alt fa-lg" onClick={() => this.props.setView('list', {})}></i>
+        </div>
         <div>
           <img src={this.state.pet.imgUrl} className="profilePictureDetail mx-auto d-block" alt="image of pet"/>
         </div>
         <h5 className="text-center text-uppercase font-weight-bold">
           {this.state.pet.name}
         </h5>
-        <div className="bg-white my-5 text-uppercase">
-          <i className="fas fa-paw ml-4"></i><span className="mr-2 p-2">{this.state.pet.breed}</span>
+        <div className="bg-white my-5 text-uppercase p-3">
+          <i className="fas fa-paw ml-4 fa-lg"></i><span className="mr-2 p-1">{this.state.pet.breed}</span>
         </div>
         <div className="bg-white my-5">
-          <i className="fas fa-birthday-cake ml-4"></i><span className="mr-2 p-2">{birthdayDate}</span>
+          <i className="fas fa-birthday-cake ml-4 fa-lg p-3"></i><span className="mr-2 p-1">{birthdayDate}</span>
         </div>
         <div className="bg-white my-5">
-          <i className="fas fa-pencil-alt ml-4"></i><span className="mr-2 p-2">{this.state.pet.description}</span>
+          <i className="fas fa-pencil-alt ml-4 fa-lg p-3"></i><span className="mr-2 p-1">{this.state.pet.description}</span>
         </div>
       </div>
     );
