b9bd81edb0bebb8ce32485858112eb70ce2d3054
Jenna Bowman <jbowman1@swarthmore.edu>
2020-08-06 06:40:44 +0000

    The Nav works and i might sob

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-export default function Homepage(props) {
-  return (
-    <div className="homepage">
-      <div className="logo">
-        <img src={'images/PetPocketV2.png'} className="logoImage"></img>
+export default class Homepage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'homepage'
+    };
+  }
+
+  render() {
+    const setView = this.props.setView;
+    return (
+      <div className="homepage">
+        <div className="logo">
+          <img src={'images/PetPocketV2.png'} className="logoImage"></img>
+        </div>
+        <div className="homepageNav">
+          <button className="petOwner" onClick={() => setView('mainMenu', {})}>PET OWNER</button>
+          <button className="petSitter" onClick={() => setView('mainMenu', {})}>PET SITTER</button>
+        </div>
       </div>
-      <div className="homepageNav">
-        <button className="petOwner">PET OWNER</button>
-        <button className="petSitter">PET SITTER</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
