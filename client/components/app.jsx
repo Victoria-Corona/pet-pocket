ac1bb574b702d9d87e1bf947ecfb12d328505b01
UnaJeon <unajeon@gmail.com>
2020-08-07 06:04:21 +0000

    completed setting the format of the profile form

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -13,7 +13,7 @@ export default class App extends React.Component {
       message: null,
       isLoading: true,
       view: {
-        name: 'homepage',
+        name: 'profileForm',
         params: {}
       }
     };
--- client/components/profileForm.jsx
+++ client/components/profileForm.jsx
@@ -5,10 +5,20 @@ export default class ProfileForm extends React.Component {
   render() {
     return (
       <div>
-        <form action="" >
-          <input type="file" className="custom-file-input"></input>
-          <label>Photo</label>
-
+        <form action="api/petProfile" method= "post" encType="multipart/form-data" >
+          <div className="form-group">
+            <div className="emptyImage mx-auto d-block mt-3" ></div>
+            <input type="file" className="form-control-file ml-5 mt-3" />
+            <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4">Name</label>
+            <input type="text" className="form-control" placeholder="Enter Name" />
+            <label htmlFor="" style={{ fontWeight: 'bold' }}>Type of Breed</label>
+            <input type="text" className="form-control" placeholder="ex.Pug"/>
+            <label htmlFor="" style={{ fontWeight: 'bold' }}>Date of Birth</label>
+            <input type="date" className="form-control" placeholder="00/00/0000"/>
+            <label htmlFor="description" style={{ fontWeight: 'bold' }}>Description</label>
+            <input type="text" className="form-control" placeholder="very friendly, snores"/>
+            <div className="d-flex justify-content-center"><button type="submit" className="nextButton mt-3" >NEXT</button></div>
+          </div>
         </form>
       </div>
     );
