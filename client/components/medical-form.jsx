1495afdbcd9b92377a2e764d41aaac4f71493684
VicCoro <corona.victoria@hotmail.com>
2020-08-11 12:26:52 -0700

    made slight changes to code

--- client/components/medical-form.jsx
+++ client/components/medical-form.jsx
@@ -40,26 +40,25 @@ class MedicalForm extends React.Component {
       },
       body: JSON.stringify(data)
     }).then(res => res.json())
-      // .then(data => console.log('success', data))
       .catch(error => console.error(error.message));
   }
 
   render() {
     return (
       <div>
-        <form action="api/pets" method="post" onSubmit={this.handleSubmit}>
+        <form action="api/pets" method="post">
           <div className="form-group">
             <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4 ml-2">Blood Type</label>
-            <input type="text" name="bloodType" className="form-control" placeholder="DEA-1.1" handleInputChange={this.handleInputChange}/>
+            <input type="text" name="bloodType" className="form-control" placeholder="DEA-1.1" onClick={this.handleInputChange}/>
             <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4 ml-2">Allergies</label>
-            <input type="text" name="allergies" className="form-control" placeholder="Optional" handleInputChange={this.handleInputChange}/>
+            <input type="text" name="allergies" className="form-control" placeholder="Optional" onClick={this.handleInputChange}/>
             <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4 ml-2">Medication</label>
-            <input type="text" name="medication" className="form-control" placeholder="Optional" handleInputChange={this.handleInputChange}/>
+            <input type="text" name="medication" className="form-control" placeholder="Optional" onClick={this.handleInputChange}/>
             <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4 ml-2">Vaccines</label>
-            <input type="text" name="vaccines" className="form-control" placeholder="Enter Vaccines" handleInputChange={this.handleInputChange}/>
+            <input type="text" name="vaccines" className="form-control" placeholder="Enter Vaccines" onClick={this.handleInputChange}/>
             <label htmlFor="" style={{ fontWeight: 'bold' }} className="mt-4 ml-2">Specialized Diet</label>
-            <input type="text" name="diet" className="form-control" placeholder="Optional" handleInputChange={this.handleInputChange}/>
-            <div className="d-flex justify-content-center"><button type="submit" className="nextButton mt-3 text-uppercase">subtmit</button></div>
+            <input type="text" name="diet" className="form-control" placeholder="Optional" onClick={this.handleInputChange}/>
+            <div className="d-flex justify-content-center"><button type="submit" className="nextButton mt-3 text-uppercase" onSubmit={this.handleSubmit}>submit</button></div>
           </div>
         </form>
       </div>
