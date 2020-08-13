01c2a9520be0d1095b54a2bbb90d2099dfddeaf3
UnaJeon <unajeon@gmail.com>
2020-08-13 07:36:19 +0000

    adding updated data

--- database/dump.sql
+++ database/dump.sql
@@ -107,8 +107,8 @@ CREATE TABLE public.pets (
     description text NOT NULL,
     "bloodType" text,
     allergies text,
-    medication text NOT NULL,
-    vaccines text NOT NULL,
+    medication text,
+    vaccines text,
     "specializedDiet" text
 );
 
@@ -331,20 +331,6 @@ COPY public."petProfile" ("petId", "userId", name, "imgUrl", breed, "dateOfBirth
 --
 
 COPY public.pets ("petId", "userId", name, "imgUrl", breed, "dateOfBirth", description, "bloodType", allergies, medication, vaccines, "specializedDiet") FROM stdin;
-
-59	1	Max	/images/petImage/shrek.jpg	cat	2020-07-29	very friendly	\N	\N	\N	\N	\N
-60	1	blue	/images/petImage/blue.jpg	bird	2020-08-04	snores	\N	\N	\N	\N	\N
-
-2	1	CK	/images/ck.jpg	Bombay	2018-11-20	picky eater, loves to cuddle, eats shoelaces	B-	N/A	N/A	FVRCP FELV FIP Rabies	Outdoor Forumla
-3	1	Twix	/images/twix.png	Maltese Poodle Mix	2015-09-01	loves friends and people	A+	Bees	Nexxguard	Bordatella Distemper Hepatitis Rabies	\N
-1	1	Buddy	/images/buddy.jpg	Pug	2016-01-04	very friendly, enjoys head pats, snores	A+	Bees	Nexxguard	Bordatella Distemper Hepatitis Rabies	Gluten Free
-37	1	Belvedere	/images/petImage/belvedere.jpg	corgi	2020-02-04	a pup	A	Fleas	Aspirin	Bordatella	soft food
-
-45	1	Draco	/images/petImage/draco.jpg	Bearded Dragon	2018-06-12	spikey	\N	grass	\N	\N	\N
-50	1	King	/images/petImage/king.jpg	Pug	2019-02-03	love him so	\N	\N	\N	\N	\N
-52	1	Jovian	/images/petImage/jovian.jpg	Lemur	1994-04-05	stared in a show	\N	\N	\N	\N	\N
-38	1	Leilani	/images/petImage/Leilani.jpg	dobermann	2020-02-04	a tiny pupper	A	fleas	\N	Bordatella	hard food
-53	1	logo	/images/petImage/petPocket.jpg	logo	2020-08-12	doesnt do much	\N	\N	\N	\N	\N
 \.
 
 
@@ -364,8 +350,8 @@ COPY public.reminder ("reminderId", name, type, description, date, "time", repea
 --
 
 COPY public.todo ("todoId", "userId", todo, "isCompleted") FROM stdin;
-5	1	fill water	\N
 6	1	change pee pad	\N
+7	1	fill water	\N
 \.
 
 
@@ -374,49 +360,6 @@ COPY public.todo ("todoId", "userId", todo, "isCompleted") FROM stdin;
 --
 
 COPY public."vetVisits" ("vetVisitId", "petId", date, reason, notes) FROM stdin;
-1	1	2019-10-10	Buddy ate a shoelace, the vet says to schedule another appointment in three days if he doesn't pass it. Urgh! So gross! True story!	Keep shoelaces and ribbons off the floor.
-2	2	2020-05-04	Had to bring CK in for stitches after getting into a fight with the neighborhood cats.	My boy won.
-3	3	2019-10-10	Twix had an ear infection.	Apply ointment and air out.
-4	37	2020-02-02	Routine checkup	
-5	37	2020-04-12	loss of appetite	add chicken and rice in meals, hand feed for three days and if condition does not change, schedule another appointment
-6	38	2020-04-15	routine checkup	
-14	37	2020-02-02	routine checkup	
-15	37	2020-03-02	throwing up	
-17	37	2020-03-04	routine	
-18	37	2020-06-12	Routine check up	Buy chewable nutritional pills
-19	37	2020-06-12	Routine check up	Buy chewable nutritional pills
-20	38	2020-02-03	routine	
-21	45	2020-03-04	routine	
-22	51	2020-02-02	routine	
-23	51	2020-03-03	routine	
-24	51	2020-04-04	eyes	
-25	50	2020-01-01	lil nose	
-26	48	2020-01-01	routine	
-27	48	2020-02-02	routine	
-28	48	2020-02-05	routine	
-29	50	2020-02-03	routine	
-30	45	2020-03-04	hang nail	
-31	45	2020-02-05	another hang nail	
-32	50	2020-02-03	routine	
-33	37	2019-11-22	checkup	
-34	37	2020-05-05	rojuiafd	
-35	45	2020-01-01	asjdkf	
-36	45	2020-06-09	asdf	
-37	38	2020-06-09	adsf	
-38	38	2020-01-01	asdf	
-39	38	2020-01-01	asdasd	
-40	45	2020-10-10	asdf	
-41	50	2020-10-10	asdf	
-42	52	2020-10-10	asdf	
-43	45	2019-12-20	asdf	
-44	53	2020-10-10	asdf	
-45	53	2020-01-01	asdf	
-46	45	2020-02-02	adsf	
-47	53	2020-02-02	asdf	
-48	45	2020-02-20	asdf	
-49	45	2019-10-13	adsf	
-50	53	2019-02-02	adsf	
-
 \.
 
 
@@ -424,8 +367,7 @@ COPY public."vetVisits" ("vetVisitId", "petId", date, reason, notes) FROM stdin;
 -- Name: pets_petId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-
-SELECT pg_catalog.setval('public."pets_petId_seq"', 60, true);
+SELECT pg_catalog.setval('public."pets_petId_seq"', 61, true);
 
 
 --
@@ -446,7 +388,7 @@ SELECT pg_catalog.setval('public."reminder_reminderId_seq"', 1, false);
 -- Name: todo_todoId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."todo_todoId_seq"', 6, true);
+SELECT pg_catalog.setval('public."todo_todoId_seq"', 7, true);
 
 
 --
@@ -461,7 +403,6 @@ SELECT pg_catalog.setval('public."todo_userId_seq"', 1, false);
 --
 
 SELECT pg_catalog.setval('public."vetVisits_vetVisitId_seq"', 33, true);
-
 
 
 --
